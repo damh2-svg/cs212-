02645a0e2bbe156c837ffb9caf07974125e58e78
Ghina <ghina.alsubaie@gmail.com>
2026-05-16 11:16:27 +0300

    Expand IEvent interface with additional methods

--- src/phase2/IEvent.java
+++ src/phase2/IEvent.java
@@ -1,5 +1,29 @@
 package phase2;
 
-public interface IEvent {
+/**
+ * Represents an event with a time slot and a location.
+ *
+ * Events are compared based on their ID.
+ */
+public interface IEvent extends Comparable<IEvent> {
 
+    /**
+     * Returns the unique ID of the event.
+     */
+    int getId();
+
+    /**
+     * Returns the time slot of the event.
+     */
+    ITimeSlot getTimeSlot();
+
+    /**
+     * Returns the location of the event.
+     */
+    ILocation getLocation();
+
+    /**
+     * Returns the set of participant IDs.
+     */
+    Set<Integer> getParticipantIds();
 }

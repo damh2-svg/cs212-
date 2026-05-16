70ee44862da98f7f981a05c91c779e0b40401aff
Ghina <ghina.alsubaie@gmail.com>
2026-05-16 11:20:09 +0300

    Implement ISchedule interface in Schedule class

--- src/phase2/Schedule.java
+++ src/phase2/Schedule.java
@@ -1,5 +1,66 @@
 package phase2;
 
-public class Schedule {
+public class Schedule implements ISchedule {
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'size'");
+    }
+
+    @Override
+    public boolean empty() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'empty'");
+    }
+
+    @Override
+    public void clear() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'clear'");
+    }
+
+    @Override
+    public boolean add(int eventId, ITimeSlot timeSlot) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+    @Override
+    public boolean remove(int eventId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+    }
+
+    @Override
+    public boolean contains(int eventId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'contains'");
+    }
+
+    @Override
+    public boolean conflicts(ITimeSlot timeSlot) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'conflicts'");
+    }
+
+    @Override
+    public Set<Integer> getEventIds() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getEventIds'");
+    }
+
+    @Override
+    public Map<ITimeSlot, Integer> getEvents() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getEvents'");
+    }
+
+    @Override
+    public void addMeeting(IMeeting meeting) throws SchedulingException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addMeeting'");
+    }
 
 }
+

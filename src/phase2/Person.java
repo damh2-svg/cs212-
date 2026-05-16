1d334e7d4c6600c4e28a4aed2113b124d9c471ed
Ghina <ghina.alsubaie@gmail.com>
2026-05-16 11:19:44 +0300

    Refactor Person class to implement IPerson interface

--- src/phase2/Person.java
+++ src/phase2/Person.java
@@ -1,5 +1,46 @@
 package phase2;
+public class Person implements IPerson {
+    
+        int personID;
+        String personName;
+        String personEmail;
+        ISchedule personSchedule;
 
-public class Person {
+        Person(int id, String name, String email, ISchedule schedule)
+        {
+                personID = id;
+                personName = name;
+                personEmail = email;
+                personSchedule = new Schedule ();
+        }
 
+        @Override
+        public int getId()
+        {
+                return personID;
+        }
+
+        @Override
+        public String getName()
+        {
+         return personName;
+        }
+
+        @Override
+        public String getEmail()
+        {
+        return personEmail;
+        }
+
+        @Override
+        public ISchedule getSchedule()
+        {
+                return personSchedule;
+        }
+
+        
+        @Override
+        public int compareTo(IPerson o) {
+                return (this.personID -  o.getId());
+        }
 }

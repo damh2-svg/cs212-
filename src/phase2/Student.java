6e718409fb7a06799bd89d89443d25135dc0289e
Ghina <ghina.alsubaie@gmail.com>
2026-05-16 11:21:17 +0300

    Update Student.java

--- src/phase2/Student.java
+++ src/phase2/Student.java
@@ -1,5 +1,18 @@
 package phase2;
 
-public class Student {
+public class Student extends Person implements IStudent {
 
+        String studentMajor;
+
+        public Student(int id, String name, String email, ISchedule schedule, String major) {
+            super(id, name, email, schedule);
+            studentMajor = major;
+        }
+
+       
+        @Override
+        public String getMajor()
+        {
+            return studentMajor;
+        }
 }

8229a12135ab62bc8bbf00dabe93f67e5fac4454
Ghina <ghina.alsubaie@gmail.com>
2026-05-16 11:18:06 +0300

    Enhance IStudent interface with documentation and inheritance
    Added documentation comments to IStudent interface and extended it from IPerson.

--- src/phase2/IStudent.java
+++ src/phase2/IStudent.java
@@ -1,5 +1,14 @@
 package phase2;
 
-public interface IStudent {
+/**
 
+    String getMajor();
+ * Represents a single student in the advising system.
+ */
+public interface IStudent extends IPerson {
+
+    /**
+     * Returns the major of the student.
+     */
+    String getMajor();
 }

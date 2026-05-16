a6195f29e2eebb50bcb0db3408496ffa42008ded
Ghina <ghina.alsubaie@gmail.com>
2026-05-16 11:18:35 +0300

    Enhance IWorkshop with JavaDoc comments
    Added JavaDoc comments to IWorkshop interface methods.

--- src/phase2/IWorkshop.java
+++ src/phase2/IWorkshop.java
@@ -1,5 +1,22 @@
 package phase2;
 
-public interface IWorkshop {
+/**
+ * Represents a workshop involving multiple advisors and students.
+ */
+public interface IWorkshop extends IEvent {
 
+    /**
+     * Returns the title of the workshop.
+     */
+    String getTitle();
+
+    /**
+     * Returns the set of advisor IDs.
+     */
+    Set<Integer> getAdvisorIds();
+
+    /**
+     * Returns the set of student IDs.
+     */
+    Set<Integer> getStudentIds();
 }

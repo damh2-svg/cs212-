97d5ba34a7c0d90fcb6baf031a2c3531bed34cf3
damh2-svg <daliamoh2005@gmail.com>
2026-05-16 20:27:57 +0300

    Refactor Person class constructor and methods
    Refactor constructor to use provided schedule and clean up method implementations.

--- src/phase2/Person.java
+++ src/phase2/Person.java
@@ -1,46 +1,42 @@
 package phase2;
+
 public class Person implements IPerson {
-    
-        int personID;
-        String personName;
-        String personEmail;
-        ISchedule personSchedule;
 
-        Person(int id, String name, String email, ISchedule schedule)
-        {
-                personID = id;
-                personName = name;
-                personEmail = email;
-                personSchedule = new Schedule ();
-        }
+    int personID;
+    String personName;
+    String personEmail;
+    ISchedule personSchedule;
 
-        @Override
-        public int getId()
-        {
-                return personID;
-        }
+    Person(int id, String name, String email, ISchedule schedule) {
 
-        @Override
-        public String getName()
-        {
-         return personName;
-        }
+        personID = id;
+        personName = name;
+        personEmail = email;
+        personSchedule = schedule;
+    }
 
-        @Override
-        public String getEmail()
-        {
+    @Override
+    public int getId() {
+        return personID;
+    }
+
+    @Override
+    public String getName() {
+        return personName;
+    }
+
+    @Override
+    public String getEmail() {
         return personEmail;
-        }
+    }
 
-        @Override
-        public ISchedule getSchedule()
-        {
-                return personSchedule;
-        }
+    @Override
+    public ISchedule getSchedule() {
+        return personSchedule;
+    }
 
-        
-        @Override
-        public int compareTo(IPerson o) {
-                return (this.personID -  o.getId());
-        }
+    @Override
+    public int compareTo(IPerson o) {
+        return this.personID - o.getId();
+    }
 }

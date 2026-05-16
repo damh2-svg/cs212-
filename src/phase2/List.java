e23cb6741e3f56f08c05d756ec2edc6d58f6c407
Ghina <ghina.alsubaie@gmail.com>
2026-05-16 11:19:08 +0300

    Generic List interface with type parameter T

--- src/phase2/List.java
+++ src/phase2/List.java
@@ -1,5 +1,21 @@
 package phase2;
 
-public interface List {
+public interface List<T> {
+    public boolean empty();
 
+    public boolean last();
+
+    public boolean full();
+
+    public void findFirst();
+
+    public void findNext();
+
+    public T retrieve();
+
+    public void update(T val);
+
+    public void insert(T val);
+
+    public void remove();
 }

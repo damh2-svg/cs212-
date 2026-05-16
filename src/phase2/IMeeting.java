97103f0554253dfdcd2e54b0108f2a78cf8deb10
Ghina <ghina.alsubaie@gmail.com>
2026-05-16 11:17:07 +0300

    Enhance IMeeting interface with method documentation
    Added documentation for IMeeting interface methods.

--- src/phase2/IMeeting.java
+++ src/phase2/IMeeting.java
@@ -1,5 +1,17 @@
 package phase2;
 
-public interface IMeeting {
+/**
+ * Represents a one-to-one meeting between an advisor and a student.
+ */
+public interface IMeeting extends IEvent {
 
+    /**
+     * Returns the advisor ID.
+     */
+    int getAdvisorId();
+
+    /**
+     * Returns the student ID.
+     */
+    int getStudentId();
 }

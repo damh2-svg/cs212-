118719f192a86aa868f84dacfac17f0e64e46155
Ghina <ghina.alsubaie@gmail.com>
2026-05-16 11:18:21 +0300

    Refactor ITimeSlot to interface with date-time methods
    Converted ITimeSlot class to an interface with methods for start and end.

--- src/phase2/ITimeSlot.java
+++ src/phase2/ITimeSlot.java
@@ -1,5 +1,21 @@
 package phase2;
 
-public class ITimeSlot {
+/**
+ * Represents a time interval defined by a start and an end date-time.
+ * Intervals are treated as half-open: [start, end), and compared as follows:
+ * - return 0 if the intervals overlap
+ * - return -1 if this interval ends before the other starts
+ * - return +1 if this interval starts after the other ends
+ */
+public interface ITimeSlot extends Comparable<ITimeSlot> {
 
+    /**
+     * Returns the start of the interval.
+     */
+    IDateTime getStart();
+
+    /**
+     * Returns the end of the interval.
+     */
+    IDateTime getEnd();
 }
